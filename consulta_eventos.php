587a29001f3aac9170d3f80ca9d9bb34f5b5733b
fabricio4135 <fabricio.alves@snowmanlabs.com>
2023-09-08 13:21:58 -0300

    ajuste na consulta

--- consulta_eventos.php
+++ consulta_eventos.php
@@ -12,6 +12,7 @@ $eventos = $databaseManager->listarEventos();
 
 <!DOCTYPE html>
 <html>
+
 <head>
     <title>Consulta de Eventos</title>
     <!-- Inclua os arquivos Bootstrap CSS e outros recursos necessários -->
@@ -29,69 +30,64 @@ $eventos = $databaseManager->listarEventos();
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css">
 
 </head>
-<body>
-<div class="mt-2" > 
-    <h1 class="text-center">Consulta de Eventos</h1>
 
-    <div class="py-5">
-        <div class="container">
-            <?php foreach ($eventos as $evento): ?>
-            <div class="row hidden-md-up">
-                <div class="col-md-4 mt-4>
+<body>
+    <div class="mt-2">
+        <h1 class="text-center">Consulta de Eventos</h1>
+    </div>
+    <div class="container py-3">
+        <div class="row">
+            <?php foreach ($eventos as $evento) : ?>
+                <div class="col-md-4 mt-4">
                     <div class="card">
                         <div class="card-block">
-                        <div class="card-header">
-                            <h4 class="card-title"><?php echo $evento['nome_evento']; ?></h4>
-                        </div>
-                        <h6 class="card-subtitle text-muted mt-2">Contato: <?php echo $evento['nome_contato']; ?>
-                         - <i class="bi bi-whatsapp"></i> <?php echo $evento['telefone_contato']; ?></h6>
-                        <p class="card-text p-y-1">
+                            <div class="card-header">
+                                <h4 class="card-title"><?php echo $evento['nome_evento']; ?></h4>
+                            </div>
+                            <h6 class="card-subtitle text-muted mt-2 px-2">Contato: <?php echo $evento['nome_contato']; ?>
+                                - <i class="bi bi-whatsapp"></i> <?php echo $evento['telefone_contato']; ?></h6>
+                            <p class="card-text p-y-1 px-2">
+                                Identificador: <?php echo $evento['evento_id']; ?> <br>
+                                Vagas Disponíveis: <?php echo $evento['vagas_disponiveis']; ?> <br>
+                                Data de Início: <?php echo $evento['data_inicio']; ?> <br>
+                                Data de Término: <?php echo $evento['data_final']; ?> <br>
+                                Número de Dias: <?php echo $evento['numero_dias']; ?> <br>
+                                Horário de Início: <?php echo $evento['horario_inicio']; ?> <br>
+                                Horário de Término: <?php echo $evento['horario_final']; ?> <br>
+                                Endereço: <?php echo $evento['endereco']; ?> <br>
+                                Localização: <?php echo $evento['localizacao']; ?> <br>
+                                <br> <br>
+                                <a href="inscricao_evento.php" class="btn btn-primary btn-sm">Inscrição</a><br><br>
+                            <h4 class="card-title text-muted p-y-1 px-2">Viagens Relacionadas: </h4><br>
 
-                        Identificador: <?php echo $evento['evento_id']; ?> <br>
-                        Vagas Disponíveis: <?php echo $evento['vagas_disponiveis']; ?> <br>
-                        Data de Início: <?php echo $evento['data_inicio']; ?> <br>
-                        Data de Término: <?php echo $evento['data_final']; ?> <br>
-                        Número de Dias: <?php echo $evento['numero_dias']; ?> <br>
-                        Horário de Início: <?php echo $evento['horario_inicio']; ?> <br>
-                        Horário de Término: <?php echo $evento['horario_final']; ?> <br>
-                        Endereço: <?php echo $evento['endereco']; ?> <br>
-                        Localização: <?php echo $evento['localizacao']; ?> <br>
-                        Nome de Contato: <?php echo $evento['nome_contato']; ?> <br>
-                        Telefone de Contato:<?php echo $evento['telefone_contato']; ?> <br> <br>
-                        <a href="inscricao_evento.php" class="btn btn-primary btn-sm">Inscrição</a><br><br>
-                       
-                        Viagens Relacionadas:
-                        <?php
-                        // Consulta as viagens relacionadas a este evento
-                        
-                        $viagens = $databaseManager->listarViagensPorEvento($evento['evento_id']);
-                        
-                        if (!empty($viagens)) {
-                            foreach ($viagens as $viagem) {
-                                echo "Data: " . $viagem['data_viagem'] . "<br>";
-                                echo "Valor da Passagem: R$ " . $viagem['valor_passagem'] . "<br>";
-                                echo "Número de Viagens: " . $viagem['numero_viagens'] . "<br>";
-                                echo "Vagas Disponíveis Ida: " . $viagem['vagas_disponiveis_ida'] . "<br>";
-                                echo "Vagas Disponíveis Volta: " . $viagem['vagas_disponiveis_volta'] . "<br><br>";
-                                echo "<a href='reserva_viagens.php' class='btn btn-success btn-sm'>Reserva</a> <br><br>";
-                            }
+                            <?php
+                            // Consulta as viagens relacionadas a este evento
+                            $viagens = $databaseManager->listarViagensPorEvento($evento['evento_id']);
+
+                            if (!empty($viagens)) {
+                                foreach ($viagens as $viagem) {
+                                    echo "<p class='card-text p-y-1 px-2'>";
+                                    echo "Data: " . $viagem['data_viagem'] . "<br>";
+                                    echo "Valor da Passagem: R$ " . $viagem['valor_passagem'] . "<br>";
+                                    echo "Número de Viagens: " . $viagem['numero_viagens'] . "<br>";
+                                    echo "Vagas Disponíveis Ida: " . $viagem['vagas_disponiveis_ida'] . "<br>";
+                                    echo "Vagas Disponíveis Volta: " . $viagem['vagas_disponiveis_volta'] . "<br><br>";
+                                    echo "<a href='reserva_viagens.php' class='btn btn-success btn-sm'>Reserva</a> <br><br>";
+                                    echo "</p>";
+                                }
                             } else {
+                                echo "<p class='card-text p-y-1 px-2'>";
                                 echo "Nenhuma viagem relacionada a este evento.";
+                                echo "</p>";
                                 echo "<br>";
                                 echo "<br>";
-                                echo "<hr>";
                             }
-                        
-                        ?>
-                            <?php  endforeach; ?>
-                            <?php $databaseManager->fecharConexao(); ?>
-
-
-                        </p>
+                            ?>
+                            </p>
                         </div>
                     </div>
                 </div>
-            </div>
+            <?php endforeach; ?>
         </div>
     </div>
 
@@ -101,4 +97,5 @@ $eventos = $databaseManager->listarEventos();
         // Você pode adicionar scripts JavaScript aqui, se necessário.
     </script>
 </body>
+
 </html>
